03ecdeb68f9b9b9aff19882fa12dfdefcfe8810c
NTEZIRYAYO Celestin <97307891+Celesin@users.noreply.github.com>
2022-07-15 22:43:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,10 @@ edjidjk xpl;kld'okl' \
                ' onsdp[sd' \
 wedkoiwef;ojkjkm ;ol;vml  l,p'; 
 
-
+euoiiaeprolk  pl[;'v[dp;x[;ldf[;sd;ld;'
+                 sdolsdp';ld;l [;'dv;ldvs\dv
+                 dkv;ld;';d],km
+                 sdksdp;askl;ll;
 rk;pld;'d]'\sxs
                'dodpop]' \
                
